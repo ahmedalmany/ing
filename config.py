21254d40e7aabc10d1125116c5a6410685ec37bc
ahmedalmany <110073513+ahmedalmany@users.noreply.github.com>
2022-07-27 07:24:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,23 +9,29 @@ if os.path.exists("local.env"):
 
 load_dotenv()
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_NAME = getenv("BOT_NAME", "song")
-API_ID = int(getenv("API_ID", "8186557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
-OWNER_NAME = getenv("OWNER_NAME", "muntazer")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ZzZzZ5")
-ALIVE_NAME = getenv("ALIVE_NAME", "muntazer")
-BOT_USERNAME = getenv("BOT_USERNAME", "")
-OWNER_ID = getenv("OWNER_ID", "5284259786")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "")
+SESSION_NAME = getenv("SESSION_NAME", "
+BAAjOfEwckUQZV66FfQ6gC1-PjwCEftbMWqC_Ps5N1V-IsyX1ddBrqJ8dHkjUL0w5MkFvTnfybDZRvmXcTzdTJfwIcx1loD3pPWWHxhqk_fJMY83cabqUotNumNMdJYtb3tQgTTs1iKkFsBftEIW1KysikFh2y2k4jTMzvyXMfHWyCC6ou3zau5-faZdrX0QUAPKu4-tTrhahap-vaULywBWORsK-eA6QutkieCOAs7tsPQWlU7u6vhcPwQx48jWsG69WnYdF8EkeMGI9OhQ6qFmiTpPFJxEX6n7657bIg0cbkT8cMP4GijBgqqE_SqH5t1HgZBut7zvdoOXue7Jm4RJAAAAAUcpU9sA")
+BOT_TOKEN = getenv("BOT_TOKEN", "
+5319187407:AAGHxgQ_CdHEExfpx6AdQEdtP8V8ruknCXk")
+BOT_NAME = getenv("BOT_NAME", "almany")
+API_ID = int(getenv("API_ID", "
+13043558"))
+API_HASH = getenv("API_HASH", "
+00cfb8246de35bfd6343a1ed7402378c")
+OWNER_NAME = getenv("OWNER_NAME", "almany")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "
+@almany_basha")
+ALIVE_NAME = getenv("ALIVE_NAME", "almany")
+BOT_USERNAME = getenv("BOT_USERNAME", "atalmany")
+OWNER_ID = getenv("OWNER_ID", "
+1715562844")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "halpralmany")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "CG_674")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "almany_masr")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "HEROKU_API_KEY")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5284259786").split()))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1715562844").split()))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES"شغل "/ تشغيل الالماني").split())
 ALIVE_IMG = getenv("ALIVE_IMG", "https://te.legra.ph/file/407ce4c57a645c11f65c0.jpg")
 START_PIC = getenv("START_PIC", "https://te.legra.ph/file/e22e5d1f0ccd57fa5f677.jpg")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
